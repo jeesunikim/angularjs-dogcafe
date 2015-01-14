ca17aafda08b8e20b196347f11594efaa7af6207
Jeesun Kim <jeesunikim@gmail.com>
2015-01-13 20:22:28 -0500

    personality and description added in app.js

--- app.js
+++ app.js
@@ -11,16 +11,18 @@
 		 };
 
 		 this.isSelected=function(checkTab){
-		 	return this.tab === checkTab;
+		 	return this.tab === checkTab; //check to see if the value matches the 'tab' parameter
 		 };
 	});
 	var puppy=[
 		{
 			breed: 'German Shepherd',
 			price: 5000,
-			canPurchase:true,
-			description:"The cutest",
+			canPurchase: true,
+			description: "The German Shepherd is a relatively new breed of dog, with their origin dating to 1899. As part of the Herding Group, German Shepherds are working dogs developed originally for herding sheep. Since that time, however, because of their strength, intelligence, trainability and obedience, German Shepherds around the world are often the preferred breed for many types of work, including search-and-rescue, police and military roles and even acting. The German Shepherd is the second-most popular breed of dog in the United States and fourth-most popular in the United Kingdom",
+			personality: "Watchful. Alert. Obedient. Loyal. Courageous. Intelligent. Curious. Confident",
 			origin: "Germany",
+			perfectfor: "Family. Kids",
 			image:
 			{
 				thumb:'img/german-shepherd.jpg'
@@ -30,8 +32,10 @@
 			breed: 'French Bulldog',
 			price: 2500,
 			canPurchase: true,
-			description:"",
+			description:'The French Bulldog is a small breed of domestic dog. "Frenchies" were the result in the 1800s of a cross between bulldog ancestors imported from England and local ratters in Paris (France).',
+			personality:"Lively. Easygoing. Affectionate. Playful. Bright. Social. Alert. Keen",
 			origin: "England",
+			perfectfor: "Family. Kids. People who live in an apartment",
 			image:
 			{
 				thumb:'img/french-bulldog.jpg'
@@ -41,8 +45,10 @@
 			breed: 'Bernese Mountain Dog',
 			price: 2500,
 			canPurchase: true,
-			description:"",
+			description:'The Bernese Mountain Dog, called in German the Berner Sennenhund, is a large sized breed of dog, one of the four breeds of Sennenhund-type dogs from the Swiss Alps. The name Sennenhund is derived from the German Senne ("alpine pasture") and Hund ("dog"), as they accompanied the alpine herders and dairymen called Senn. Berner (or Bernese in English) refers to the area of the breed’s origin, in the canton of Bern in Switzerland. This mountain dog was originally kept as a general farm dog. Large Sennenhunde in the past were also used as draft animals, pulling carts.',
+			personality:"Loyal. Faithful. Intelligent. Affectionate",
 			origin: "Switzerland",
+			perfectfor: "Family. Kids.",
 			image:
 			{
 				thumb:'img/bernese.jpg'
@@ -52,41 +58,13 @@
 			breed: 'Chihuahua',
 			price: 1500,
 			canPurchase: true,
-			description:"",
+			description:"the smallest breed of dog and is named for the state of Chihuahua in Mexico. Chihuahuas come in a wide variety of sizes, head shapes, colors, and coat lengths.",
+			personality:"Courageous. Lively. Devoted. Alert. Quick",
+			perfectfor: "Adults",
 			origin: "Mexico",
 			image:
 			{
 				thumb:'img/chihuahua.jpg'
-			}},
-			{
-			breed: 'Cat',
-			price: 500,
-			canPurchase: true,
-			description:"",
-			origin: "no clue",
-			image:
-			{
-				thumb:'img/kitten.jpg'
-			}},
-			{
-			breed: 'Cute Cat',
-			price: 0,
-			canPurchase: true,
-			description:"",
-			origin: "unknown",
-			image:
-			{
-				thumb:'img/kitten-2.jpg'
-			}},
-			{
-			breed: 'Shrek Cat',
-			price: 5000,
-			canPurchase: true,
-			description:"",
-			origin:"Far Far Away",
-			image:
-			{
-				thumb:'img/kitten-3.jpg'
 			}
 		}]
 })();
